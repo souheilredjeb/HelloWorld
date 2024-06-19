37a59873611890204576aa3bd08bbc0c851ee4ed
ASEJ1 <ahmedsaifeddinejaffel@gmail.com>
2024-06-19 09:29:15 +0100

    hello

---
+++ src/com/greetings/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.greetings;
+
+public class HelloWorld 
+
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub	
+		System.out.println("Hello World "+new java.util.Date());
+
+	}
+
+}
